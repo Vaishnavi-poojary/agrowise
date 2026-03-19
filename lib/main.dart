98cf8ed015d72007c1943840f38bc98ad6d64de1
VAISHNAVI <vaishnavipoojary14@gmail.com>
2026-03-19 13:49:42 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ import 'dart:async';
 // ─── CONFIGURATION ──────────────────────────────────────────
 const String BASE_URL = 'https://agrowise.onrender.com';
 const String WEATHER_API_KEY = '45b3f6baa2b6ea5ca19624f7d83b1317';
+const String NEWS_API_KEY    = 'e97459942afb4012b8a99a210e53b4c9';
+
 
 // ─── LANGUAGE SUPPORT ───────────────────────────────────────
 enum AppLanguage { english, kannada, hindi, tamil, telugu }
