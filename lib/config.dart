e356389960e9a2c53a3ccb6120e98510ae0817ab
VAISHNAVI <vaishnavipoojary14@gmail.com>
2026-03-19 14:27:17 +0530

    Add FastAPI crop recommendation app

---
+++ lib/config.dart
@@ -0,0 +1,3 @@
+const String BASE_URL = 'https://agrowise.onrender.com';
+const String WEATHER_API_KEY = '45b3f6baa2b6ea5ca19624f7d83b1317';
+const String NEWS_API_KEY = 'e97459942afb4012b8a99a210e53b4c9';
